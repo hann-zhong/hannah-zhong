2ffa429d853573764550857ef57890fa8a96b5c4
Hannah <hzhong02@gmail.com>
2021-05-30 07:25:03 -0400

    feat: copy for projects and work

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -9,7 +9,9 @@ import img from '../assets/placeholder.png';
 
 function Work() : JSX.Element {
 
-    const workText = 'some cool places i\'ve been';
+    const workText = 'I\'ve been fortunate enough to have the opportunities to work at some ' +
+    'amazing places with some even more amazing people. I\'ve delivered impactful products, ' +
+    'collaborated with a diverse group of people, and never once had to get coffee! These experiences have truly shaped my path as an engineer.';
     return (
         <Layout>
             <div id='work' className='block'>
