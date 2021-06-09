f0172bc7dff2caef81c899a074be93a577f170bf
Hannah <hzhong02@gmail.com>
2021-06-09 18:14:17 -0400

    feat: work cards and copy for section

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from './Layout'
 import Text from './fundamentals/Text';
 import Image from './fundamentals/Image';
+import WorkCard from './fundamentals/WorkCard';
 import './styles/Work.css';
 import './styles/global.css';
 
@@ -10,18 +11,53 @@ import img from '../assets/placeholder.png';
 function Work() : JSX.Element {
 
     const workText = 'In the past, I\'ve gotten to work with diverse groups of people while delivering impactful products. ' +
-    'You know, making cool things with computers and stuff. Here\'s where I\'ve worked:';
+    'You know, making cool things with computers and stuff. Here\'s a gist of what I\'ve done:';
     return (
         <Layout>
-            <div id='work' className='block'>
-                <Text 
-                    heading={'work.'}
-                    text={workText} 
-                />
-                <Image 
-                    url={img} 
-                    alt={'placeholder'} 
-                />
+            <div id='work'>
+                <div className='block'>
+                    <Text 
+                        heading={'work.'}
+                        text={workText} 
+                    />
+                    <Image 
+                        url={img} 
+                        alt={'placeholder'} 
+                    />
+                </div>
+                <div className='workBody'>
+                <WorkCard 
+                        name={'ORACLE'} 
+                        color={'#DA8E7F'}
+                        h={230}
+                        loc={'Seattle, WA'}
+                        dates={'June - Sept. 2021'}
+                        position={'Incoming Software Engineer Intern'}
+                        desc1={'Oracle Cloud Infrastructure (OCI).'}
+                        desc2={'Come back later!! 😉'}
+                    />
+                    <WorkCard 
+                        name={'CHARIPAY'} 
+                        color={'#9DE1F0'}
+                        h={230}
+                        loc={'Los Angeles, CA'}
+                        dates={'Apr. 2020 - May 2021'}
+                        position={'Web Developer'}
+                        desc1={'Helped to create frontend for COVID-19 donation portal, which faciliated the distribution of 200K+ PPE.'}
+                        desc2={'Developed responsive pages for company website based on Figma wireframes in React;' + 
+                            ' wrote scripts for a confirmation email service.'}
+                    />
+                    <WorkCard 
+                        name={'FIDELITY INVESTMENTS'} 
+                        color={'#B2CD8C'}
+                        h={264}
+                        loc={'Smithfield, RI'}
+                        dates={'June - Aug. 2020'}
+                        position={'Software Engineer Intern'}
+                        desc1={'Implented RESTful APIs to extract and manipulate relational database information. Modified functions to support additional business logic.'}
+                        desc2={'Wrote unit tests with 100% code coverage and service automation scripts.'}
+                    />
+                </div>
             </div>
         </Layout>
     );
--- src/components/fundamentals/WorkCard.tsx
+++ src/components/fundamentals/WorkCard.tsx
@@ -1,17 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import './styles/WorkCard.css';
-import './styles/global.css';
+import '../styles/WorkCard.css';
+import '../styles/global.css';
 
 interface WorkCardProps {
-    width: number;
-    height: number;
     name: string
+    color: string
+    h: number
+    loc: string
+    dates: string
+    position: string
+    desc1: string
+    desc2: string
 }
 
 function WorkCard(props:WorkCardProps) : JSX.Element {
-    const {width, height, name} = props;
+    const { name, color, h, loc, dates, position, desc1, desc2 } = props;
     return (
-        <div>{name}</div>
+        <div style={{height: h+75}} className='workBkgd'>
+            <div style={{backgroundColor: color, height: h}} className='compBkgd'>
+                    <p className='companyName'>{name}</p>
+                    <p className='workInfo'>{loc}</p>
+                    <p className='workInfo'>{dates}</p>
+
+            </div>
+            <div className='workDesc'>
+                <div className='workCard'>
+                    <p>{position}</p>
+                </div>  
+                <ul>
+                    <li className='workDescItems'>{desc1}</li>
+                    <li className='workDescItems'>{desc2}</li>
+                </ul>
+            </div>
+        </div>
     );
 }
 
